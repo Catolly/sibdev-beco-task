2680bdda1ffb80ac2f4da97053ebd9b011927840
Catolly <45347427+Catolly@users.noreply.github.com>
2021-06-01 04:22:11 +0700

    upload full task

---
+++ readme.md
@@ -0,0 +1,2 @@
+npm run build - сборка проекта в production 
+npm run dev - сборка проекта в development mode в режиме watch
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.sass$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ]
+  }
+}
